2f8acd44df5ccee774a08da5de9f5491d110c399
ADARSH SHARMA <sharmastudy27@gmail.com>
2026-02-25 23:20:55 +0530

    sol

---
+++ Starters 227/Fonta.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin>>s;
+    if(s[4]=='a' && s[3]=='t' && s[2]=='n'){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+
+}
+
+// Successfully Submitted
